b07d501f48e00ea3343980d477d04aa3e1e5f7ab
OriginalNam3 <chanmingchau@gmail.com>
2022-02-05 21:33:02 +0000

    Reformatting
    Organising by c++ and python

---
+++ C++/CodeForces/1594D.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct person {
+    bool searched = false;
+    vector<int> sus;
+    vector<int> homies;
+};
+
+
+
+int count(int grid){
+
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--){
+        int a, b, c;
+        cin >> a >> b >> c;
+        c = c == 'imposters';
+        
+        }
+
+    }
+    return 0;
+}
---
+++ C++/GoogleHashcode/OnePizza/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int c;
+    cin >> c;
+
+    return 0;
+}
